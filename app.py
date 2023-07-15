e4e3b972eb1ec576073b35155f2753ede0dab15a
Mark Hobson <markhobson@gmail.com>
2023-07-15 16:47:32 +0100

    Extract product repository

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+class ProductRepository:
+    def get_all(self):
+        return ["a", "b", "c"]
+
 @app.route("/")
 def index():
-    products = ["a", "b", "c"]
+    products = ProductRepository().get_all()
     return f"<p>{products}</p>"
