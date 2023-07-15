1a50208b424c1a3d140b37df6af427a324ecd6f9
Mark Hobson <markhobson@gmail.com>
2023-07-15 16:46:57 +0100

    Display list of products

--- app.py
+++ app.py
@@ -3,5 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    products = ["a", "b", "c"]
+    return f"<p>{products}</p>"
