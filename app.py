0e3350539aaaf8e178ec009888c3d5b4a607851c
Mark Hobson <markhobson@gmail.com>
2023-07-17 12:56:56 +0100

    Use class-based views

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import inject
 from flask import Flask
+from flask.views import View
 
 app = Flask(__name__)
 
@@ -7,10 +8,15 @@ class ProductRepository:
     def get_all(self):
         return ["a", "b", "c"]
 
-@app.route("/")
-@inject.autoparams()
-def index(product_repository: ProductRepository):
-    products = product_repository.get_all()
-    return f"<p>{products}</p>"
+class ProductView(View):
+    @inject.autoparams()
+    def __init__(self, product_repository: ProductRepository):
+        self.product_repository = product_repository
+
+    def dispatch_request(self):
+        products = self.product_repository.get_all()
+        return f"<p>{products}</p>"
+
+app.add_url_rule("/", view_func=ProductView.as_view("index"))
 
 inject.configure()
