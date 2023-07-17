4900e60e2331df3f6d29eb02a9574e132c5eb2ec
Mark Hobson <markhobson@gmail.com>
2023-07-17 17:10:02 +0100

    Teardown fake container in integration tests

--- tests/integration/test_app.py
+++ tests/integration/test_app.py
@@ -1,3 +1,4 @@
+import pytest
 import inject
 from app.products import ProductRepository
 
@@ -7,11 +8,16 @@ class FakeProductRepository:
         return ["x", "y", "z"]
 
 
-def test_index(client):
+@pytest.fixture()
+def container():
     inject.clear_and_configure(lambda binder: binder
         .bind(ProductRepository, FakeProductRepository())
     )
+    yield
+    inject.clear()
 
+
+def test_index(client, container):
     response = client.get("/")
 
     assert b"<p>['x', 'y', 'z']</p>" in response.data
