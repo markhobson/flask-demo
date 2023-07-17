2ce23b47bf51d45fb3117662a6277e4ef97da060
Mark Hobson <markhobson@gmail.com>
2023-07-17 13:18:40 +0100

    Unit test index with mocks

---
+++ tests/unit/test_unit_app.py
@@ -0,0 +1,11 @@
+from unittest.mock import Mock
+from app import ProductView
+
+def test_index():
+    product_repository = Mock()
+    product_repository.get_all = Mock(return_value=["x", "y", "z"])
+    view = ProductView(product_repository)
+
+    response = view.dispatch_request()
+
+    assert response == "<p>['x', 'y', 'z']</p>"
