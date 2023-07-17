c568c66ccb55cbe38c6c3d307b11dd55506972d3
Mark Hobson <markhobson@gmail.com>
2023-07-17 14:24:39 +0100

    Configure container before creating app

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,9 @@ from products import ProductView
 
 
 def create_app():
+    inject.configure()
+
     app = Flask(__name__)
     app.add_url_rule("/", view_func=ProductView.as_view("index"))
-
-    inject.configure()
 
     return app
