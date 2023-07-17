8db4b43e06c843e8320e15b438bb2d7713810598
Mark Hobson <markhobson@gmail.com>
2023-07-17 17:52:30 +0100

    Use test class for product view

--- tests/unit/test_products.py
+++ tests/unit/test_products.py
@@ -6,21 +6,21 @@ from jinja2 import FileSystemLoader
 from app.products import ProductView
 
 
-@pytest.fixture()
-def app():
-    app = Flask("test")
-    app.jinja_loader = FileSystemLoader(f"{os.getcwd()}/app/templates")
-    return app
+class TestProductView:
+    @pytest.fixture()
+    def app(self):
+        app = Flask("test")
+        app.jinja_loader = FileSystemLoader(f"{os.getcwd()}/app/templates")
+        return app
 
+    def test_index(self, app):
+        product_repository = Mock()
+        product_repository.get_all = Mock(return_value=["x", "y", "z"])
+        view = ProductView(product_repository)
 
-def test_index(app):
-    product_repository = Mock()
-    product_repository.get_all = Mock(return_value=["x", "y", "z"])
-    view = ProductView(product_repository)
+        with app.app_context():
+            response = view.dispatch_request()
 
-    with app.app_context():
-        response = view.dispatch_request()
-
-    assert "<li>x</li>" in response \
-        and "<li>y</li>" in response \
-        and "<li>z</li>" in response
+        assert "<li>x</li>" in response \
+            and "<li>y</li>" in response \
+            and "<li>z</li>" in response
