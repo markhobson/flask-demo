b63d82d75b92b65ef354b68f45046079230a1988
Mark Hobson <markhobson@gmail.com>
2023-07-18 13:37:13 +0100

    Don't unit test views, use integration tests instead
    Too much work fighting the framework to create a working Flask environment.
    Integration tests do that much better instead.

--- tests/unit/test_products.py
+++ tests/unit/test_products.py
@@ -1,5 +1,4 @@
-from unittest.mock import Mock
-from app.products import Product, ProductRepository, index
+from app.products import Product, ProductRepository
 
 
 class TestProductRepository:
@@ -9,15 +8,3 @@ class TestProductRepository:
         products = product_repository.get_all()
 
         assert products == [Product("Apple"), Product("Banana"), Product("Carrot")]
-
-
-def test_index(app):
-    product_repository = Mock()
-    product_repository.get_all = Mock(return_value=[Product("x"), Product("y"), Product("z")])
-
-    with app.app_context():
-        response = index(product_repository)
-
-    assert "<li>x</li>" in response \
-        and "<li>y</li>" in response \
-        and "<li>z</li>" in response
