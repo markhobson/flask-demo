b3c153bb5c67ff8964be6eea78c3f7d11c941a87
Mark Hobson <markhobson@gmail.com>
2023-10-15 16:00:01 +0100

    Narrow scope of e2e live server to package

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -7,12 +7,12 @@ from flask import Flask
 from app import create_app
 
 
-@pytest.fixture(name="container")
+@pytest.fixture(name="container", scope="package")
 def container_fixture() -> Generator[None, None, None]:
     yield
     inject.clear()
 
 
-@pytest.fixture(name="app", scope="session")
+@pytest.fixture(name="app", scope="package")
 def app_fixture() -> Flask:
     return create_app()
